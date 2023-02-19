d8349556a96e74684b0f8ff35fefa3ffce4b9b94
Maor Kima <mkima@apple.com>
2023-02-19 08:55:45 +0200

    date change

--- timewatch/timewatch.py
+++ timewatch/timewatch.py
@@ -116,7 +116,12 @@ class TimeWatch:
         all_records = pd.DataFrame(l)
         all_records.columns = header
         all_records['סה"כ שעות'] = all_records['סה"כ שעות'].astype(float)
-        all_records.to_csv(r'\tmp\test.csv')
+        import tempfile
+
+        tempdir = tempfile.gettempdir()
+        file = tempdir.join(r'hours_report.csv')
+        print('report file at:'+file)
+        all_records.to_csv(file)
         sum = all_records.groupby('משימה')['סה"כ שעות'].sum()
         total = sum.sum()
         print(sum.to_string())
